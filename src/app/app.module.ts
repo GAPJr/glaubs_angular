f2739fb025cbe2dbef36a3d7db5e6e41b53e0107
Pereira Glauberio (PS/ESY2-LA) <glauberio.pereira@br.bosch.com>
2021-11-08 19:11:44 -0300

    add server comp remove app comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ServerComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ServerComponent]
 })
 export class AppModule { }
